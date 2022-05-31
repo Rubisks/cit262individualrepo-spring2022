aedf83c239bec2de7e83569e303bef0524794654
Tuker Strand <strand.tucker@gmail.com>
2022-05-31 11:12:57 -0600

    adding a deployment enviorment

--- test/loadbalancer.test.js
+++ test/loadbalancer.test.js
@@ -2,7 +2,7 @@ import fetch from 'node-fetch';
 import assert from 'assert';
 
 it ('should get Hello', async ()=>{
-    const answer = await fetch('http://35.243.132.37');
-    assert.equal(answer,"Hello");
-    
+    const responce  = await fetch('http://35.243.132.37');
+    const text = await responce.text();
+    assert.equal(text,"Hello");
 })
